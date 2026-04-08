bdd88d170eca9c59ae95f258878854d17d8a7fd3
陳佳沛 <154941207+Chiapei-Chen@users.noreply.github.com>
2026-04-08 12:27:19 +0800

    feat:add web url

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://market-pulse-web.zeabur.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
